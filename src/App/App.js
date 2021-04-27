3dc9fca3370d559dbc6f8a3ca965144713a51cb3
Nada <ouji.nada@gmail.com>
2021-04-27 15:29:45 +0200

    composant props + styles dynamiques

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 const uneVar='Bonjour Paris';
 
 function App() {
   return (
     <div className="App">
-      {uneVar}
+     <Button label="Ok" lorsqueJeClickSurLeBoutton={(evt) => {
+       console.log("j\'ai ete clicke onc fais qqch stp !!");
+       alert('Button clicked!!!!'); // jamais utiliser alert ça bloque tout
+       console.log('L\'alert est fermée ');
+     }} />
+     <Button label="Cancel"  CouleurDeFond={'tomato'} taillePolice={24} lorsqueJeClickSurLeBoutton={(evt) => {}} />
+     <Button label="Don't know" CouleurDeFond="skyblue" lorsqueJeClickSurLeBoutton={(evt) => {}} />
     </div>
   );
 }
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import './Button.css'
 
-const Button =() =>{
-    return <div> Hello world !! </div>
-}
+const Button =(props) => {
+   console.log(props); // ça me retourne label et l'objet lorsque je clique 
+   // return <div onClick={props.lorsqueJeClickSurLeBoutton} > {props.label} </div> // Mauvaise pratique
+   return <div style={{backgroundColor:props.CouleurDeFond, fontSize:props.taillePolice+'px'}} className="Button" onClick={(evt) => {props.lorsqueJeClickSurLeBoutton(true)}} > {props.label} </div> // Bonne pratique -> creation d'une arrow 
+    // {{}} -> injecter du js {} et la deuxieme pour declarer un object {}
+};
+
+//'avantage de l'arrow function est de garder le contexte
+// mais dans ce cas de figure c'est la même chose que function 
+export default Button;
